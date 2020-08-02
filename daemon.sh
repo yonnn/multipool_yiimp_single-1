ceb99df1a91a4cb4bc886801aa956bb22682a406
gnark-mining <46746362+gnark-mining@users.noreply.github.com>
2020-08-02 19:27:23 -0400

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -3,6 +3,7 @@
 #####################################################
 
 source /etc/functions.sh
+source /etc/multipool.conf 
 source $STORAGE_ROOT/yiimp/.yiimp.conf
 
 echo -e " Installing BitCoin PPA...$COL_RESET"
@@ -33,7 +34,7 @@ echo -e "$GREEN Berkeley 4.8 Completed...$COL_RESET"
 
 echo -e " Building Berkeley 5.3, this may take several minutes...$COL_RESET"
 sudo mkdir -p $STORAGE_ROOT/berkeley/db5/
-hide_output sudo wget 'http://download.oracle.com/berkeley-db/db-5.3.28.tar.gz'
+hide_output sudo wget 'http://anduin.linuxfromscratch.org/BLFS/bdb/db-5.3.28.tar.gz'
 hide_output sudo tar -xzvf db-5.3.28.tar.gz
 cd db-5.3.28/build_unix/
 hide_output sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$STORAGE_ROOT/berkeley/db5/
