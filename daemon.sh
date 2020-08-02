79cf01f0e8fef01b73e90a9e96f776ca6df15b0d
gnark-mining <46746362+gnark-mining@users.noreply.github.com>
2020-08-02 18:49:36 -0400

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -1,27 +1,41 @@
+#!/usr/bin/env bash
+
 #####################################################
 # Created by cryptopool.builders for crypto use...
 #####################################################
 
 source /etc/functions.sh
+source /etc/multipool.conf
 source $STORAGE_ROOT/yiimp/.yiimp.conf
+source $HOME/multipool/yiimp_single/.wireguard.install.cnf
+
+set -eu -o pipefail
+
+function print_error {
+    read line file <<<$(caller)
+    echo "An error occurred in line $line of file $file:" >&2
+    sed "${line}q;d" "$file" >&2
+}
+trap print_error ERR
+
 
 echo -e " Installing BitCoin PPA...$COL_RESET"
 if [ ! -f /etc/apt/sources.list.d/bitcoin.list ]; then
 hide_output sudo add-apt-repository -y ppa:bitcoin/bitcoin
 fi
+
 echo -e " Installing additional system files required for daemons...$COL_RESET"
 hide_output sudo apt-get update
 apt_install build-essential libtool autotools-dev \
 automake pkg-config libssl-dev libevent-dev bsdmainutils git libboost-all-dev libminiupnpc-dev \
 libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev \
-protobuf-compiler libqrencode-dev libzmq3-dev
+protobuf-compiler libqrencode-dev libzmq3-dev libgmp-dev
 
 sudo mkdir -p $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 echo -e "$GREEN Additional System Files Completed...$COL_RESET"
 
 echo -e " Building Berkeley 4.8, this may take several minutes...$COL_RESET"
-hide_output sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 sudo mkdir -p $STORAGE_ROOT/berkeley/db4/
 hide_output sudo wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
 hide_output sudo tar -xzvf db-4.8.30.NC.tar.gz
@@ -32,12 +46,23 @@ cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 sudo rm -r db-4.8.30.NC.tar.gz db-4.8.30.NC
 echo -e "$GREEN Berkeley 4.8 Completed...$COL_RESET"
 
-echo -e " Building Berkeley 5.3, this may take several minutes...$COL_RESET"
+echo -e " Building Berkeley 5.1, this may take several minutes...$COL_RESET"
 sudo mkdir -p $STORAGE_ROOT/berkeley/db5/
-hide_output sudo wget 'http://download.oracle.com/berkeley-db/db-5.3.28.tar.gz'
+hide_output sudo wget 'http://download.oracle.com/berkeley-db/db-5.1.29.tar.gz'
+hide_output sudo tar -xzvf db-5.1.29.tar.gz
+cd db-5.1.29/build_unix/
+hide_output sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$STORAGE_ROOT/berkeley/db5/
+hide_output sudo make install
+cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
+sudo rm -r db-5.1.29.tar.gz db-5.1.29
+echo -e "$GREEN Berkeley 5.1 Completed...$COL_RESET"
+
+echo -e " Building Berkeley 5.3, this may take several minutes...$COL_RESET"
+sudo mkdir -p $STORAGE_ROOT/berkeley/db5.3/
+hide_output sudo wget 'http://anduin.linuxfromscratch.org/BLFS/bdb/db-5.3.28.tar.gz'
 hide_output sudo tar -xzvf db-5.3.28.tar.gz
 cd db-5.3.28/build_unix/
-hide_output sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$STORAGE_ROOT/berkeley/db5/
+hide_output sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$STORAGE_ROOT/berkeley/db5.3/
 hide_output sudo make install
 cd $STORAGE_ROOT/yiimp/yiimp_setup/tmp
 sudo rm -r db-5.3.28.tar.gz db-5.3.28
