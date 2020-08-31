8c0868c4c9794f3935487d608bce36db6b2e0630
CubicStake <quentin.neveu@hotmail.ca>
2020-08-31 17:01:31 -0400

    d

--- system.sh
+++ system.sh
@@ -124,7 +124,7 @@ build-essential libtool autotools-dev automake pkg-config libevent-dev bsdmainut
 
 
 echo -e "$GREEN Done...$COL_RESET"
-echo -e " Downloading gnark-mining YiiMP Repo...$COL_RESET"
+echo -e " Downloading Original-Tasty YiiMP Repo...$COL_RESET"
 hide_output sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 if [[ ("$CoinPort" == "Yes") ]]; then
 	cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
