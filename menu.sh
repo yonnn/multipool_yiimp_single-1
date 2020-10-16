6ba3546d8990df63e6c84eeb40f5958b34e0116c
dongpu1813 <42663880+dongpu1813@users.noreply.github.com>
2020-10-16 21:07:29 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -5,7 +5,7 @@
 
 source /etc/functions.sh
 
-RESULT=$(dialog --stdout --default-item 1 --title "Ultimate Original-Tasty Setup Installer v1.70" --menu "Choose one" -1 60 5 \
+RESULT=$(dialog --stdout --default-item 1 --title "Ultimate Original-Tasty Setup Installer v1.39" --menu "Choose one" -1 60 5 \
 ' ' "- For small private pools -" \
 1 "YiiMP - Single Server" \
 ' ' "- If you plan on adding more servers later -" \
